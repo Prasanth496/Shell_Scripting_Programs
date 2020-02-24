e926db1436fa15731beed06060936dbe6987c41d
Prasanth496 <saiprashanthreddy96@gmail.com>
2020-02-24 17:41:23 +0530

    [prasanth].refactor deleted duplicate files from master

--- BasicCorePrograms/userinput.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-read -p "Enter the name:" name
-if [ ${#name} -ge 3 ]
-then
-echo "Hello $name, How are you?"
-else
-echo enter the proper name
-fi
--- FunctionalPrograms/Distance.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-read -p "Enter the distance of x:" x
-read -p "Enter the distance of y:" y
-num=$( awk -v a=$x -v b=$y 'BEGIN { print (((a*a)+(b*b))**0.5) }' )
-echo "The distance from origin(0,0) is $num"
